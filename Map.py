8e48366eaa220f1c67e0a4445b559a34321fde2e
PahanSoloEasy <skokovvova2@gmail.com>
2026-02-10 18:13:49 +0300

    Красивый комит

--- Map.py
+++ Map.py
@@ -7,7 +7,6 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
 
-
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
